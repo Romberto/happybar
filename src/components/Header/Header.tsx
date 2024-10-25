771bb3bcab322eae7692c5b3aefa13728d550ae8
Romberto <korhoff@mail.ru>
2024-10-25 14:22:19 +0300

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <header >
+            <div className="header__inner grid grid-cols-2 md:grig-cols-3 w-full">
+                
+            </div>
+        </header>
+    );
+}
+
+export default Header;
